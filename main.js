66ddbca127fd5bd4c70acbd122c4a2b211f253e0
EC2 Default User <ec2-user@ip-172-31-12-243.us-east-2.compute.internal>
2018-08-12 00:19:07 +0000

    modify index.html main.js

--- main.js
+++ main.js
@@ -12,53 +12,91 @@ $(function() {
     return shift(Math.round(shift(number, precision, false)), precision, true);
   }
 
-
-  var a = $("#para_a").val();
-  var b = $("#para_b").val();
-  var n = $("#para_n").val();
-  var delta = (b - a) / n;
-
-  var x = Number(a);
-  var str_buff = "";
-  var ans = 0;
-  var ans_sum = 0;
-  
-  for ( var i = 1; i <= n; i++ ) {
-    x = i*delta;
-    ans = Math.sqrt(x)*delta;
-    str_buff += round(ans, 2).toString();
-    if (i != n) {
-      str_buff += " + ";
-    }
-    ans_sum += ans;
-  } 
-  $("#ans_sum").html("\\( \\simeq " + str_buff + "\\) \n" + "\\( = " + round(ans_sum, 2).toString() + "\\)");
-
-
   // 計算ボタン押下時
   $(".cal-form").submit(function() {
-    var a = $("#para_a").val();
-    var b = $("#para_b").val();
-    var n = $("#para_n").val();
+    var a = Number($("#para_a").val());
+    var b = Number($("#para_b").val());
+    var n = Number($("#para_n").val());
     var delta = (b - a) / n;
   
-    var x = Number(a);
     var str_buff = "";
-    var ans = 0;
-    var ans_sum = 0;
+    var x = 0.0;
+    var x1 = 0.0;
+    var x2 = 0.0;
+    var y = 0.0;
+    var y_sum = 0.0;
+    var kai1 = [];
+    var kai2 = [];
+    var area = 0.0;
+    var area_sum1 = 0.0;
+    var area_sum2 = 0.0;
     
+    // 厳密解（に近い近似解）→できれば厳密解をプロットしたい
+    for ( var i = 1; i <= 100*n; i++) {
+      x = a + i*(delta/100);
+      y = Math.sqrt(x);
+      area = y*(delta/100);
+      area_sum1 += area;
+      kai1.push({x,y});
+    }
+    
+    // 級数和
     for ( var i = 1; i <= n; i++ ) {
-      x = i*delta;
-      ans = Math.sqrt(x)*delta;
-      str_buff += round(ans, 2).toString();
+      x1 = a + (i-1)*delta;
+      x2 = a + i*delta;
+      y = Math.sqrt(x2)
+      area = y*delta;
+      area_sum2 += area;
+      x = x1;
+      kai2.push({x,y});
+      x = x2;
+      kai2.push({x,y});
+      str_buff += round(y, 2).toString();
       if (i != n) {
         str_buff += " + ";
       }
-      ans_sum += ans;
-    } 
-    $("#ans_sum").html("\\( \\simeq " + str_buff + "\\) \n" + "\\( = " + round(ans_sum, 2).toString() + "\\)");
+    }
+    $("#ans_sum").html("\\( \\simeq " + str_buff + "\\) \n" + "\\( = " + round(area_sum2, 2).toString() + "\\)");
     
-     // 計算してグラフを書く関数を呼ぶ
+    // 計算してグラフを書く関数を呼ぶ
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var myChart = new Chart(ctx, {
+      type: 'scatter',
+      data: {
+        //labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
+        datasets: [
+        {
+          type: 'scatter',
+          label: '厳密解',
+          //data: [{x:0,y:0}, {x:10,y:10}],
+          data: kai1,
+          cubicInterpolationMode: 'monotone',
+          borderColor: "red",
+          pointRadius: 0.1, // 点の半径
+          //backgroundColor: "rgba(153,0,51,0.4)"
+        },
+        {
+          type: 'scatter',
+          label: '近似解',
+          //data: [{x:0,y:1},{x:1,y:1},{x:1,y:2},{x:2,y:2}],
+          data: kai2,
+          pointRadius: 0, // 点の半径
+          cubicInterpolationMode: 'monotone',
+          backgroundColor: "rgba(153,255,51,1)",
+        },
+        
+        ]
+      },
+      options: {
+        elements: {
+          line:     {
+            // ベジェ曲線を無効化し、直線で接続する
+            tension: 0,
+          }
+        }
+      }
+    });
+    
     return false;
   });
 
